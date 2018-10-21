54b0dce794d07bca66d1b5cb4c7bf1c186df7239
bharat-s <44318923+bharat-s@users.noreply.github.com>
2018-10-21 01:10:06 -0400

    so yeah thats it

--- neoview.py
+++ neoview.py
@@ -1,17 +1,13 @@
 from astropy.io import fits
 
-
-def load_file(f='data/ASTRO/2018/285/46P/FINE_POINT/NEOS_SCI_2018285081000_clean.fits'):
+def load_file(f=r'D:\NEOSSAT Data\ASTRO\2018\033\2002_AJ129\FINE_POINT\NEOS_SCI_2018033163030_clean.fits'):
     image = fits.open(f)
     image.info()
 
-
 def main():
-    load_file()
+   load_file()
 
 if __name__ == "__main__":
     main()
 
-
-
-
+##Hi this is to track changes
