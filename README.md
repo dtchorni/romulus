170368d3a3d9a2c6a2255bafa4fc0802df83e6db
dtchorni <dtchorni@uwaterloo.ca>
2018-10-21 10:23:54 -0400

    finalizing

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# romulus
-Space apps 2018
+# Romulus - Space apps 2018 Waterloo ON
+##Challenge Description
+We proposed our own problem combining CSA's challenge in finding Near Earth Objects
+from Canada's very own satellite, NEOSSAT. Additionally, we took the concepts from
+ NASA's Moon rover mission challenge to plan rover/probe missions to NEO's given their trajectory.
+
--- neoview.py
+++ neoview.py
@@ -1,52 +1,73 @@
 from ftplib import FTP
 import sys
+import os
 
 
-def load_file(f='data/ASTRO/2018/285/46P/FINE_POINT/NEOS_SCI_2018285081000_clean.fits'):
-    global image
-    image = fits.open(f)
-    image.info()
+# def load_file(f='data/ASTRO/2018/285/46P/FINE_POINT/NEOS_SCI_2018285081000_clean.fits'):
+#     global image
+#     image = fits.open(f)
+#     image.info()
 
 
 
 def print_help(ftp=None,tokens=[]):
-    print('Usage:\n\thelp : print this dialog'
+    print('\nUsage:\n\thelp : print this dialog'
           '\n\tls : list current directory'
           '\n\tcd <dir> : change into <dir>'
           '\n\tuse <dir> : use <dir> to download *clean.fits for processing'
-          '\n\tq : quit')
+          '\n\tq : quit\n\n')
 
 def list_dir(ftp,tokens=[]):
     ftp.retrlines('LIST')
+    #[print(f) for f in ftp.nlst()]
 
 def change_dir(ftp,tokens):
     if len(tokens)==1:
         print('Error: missing dir')
     else:
+        tokens[1]=tokens[1][:-1]
+        print(tokens[1])
         ftp.cwd(tokens[1])
 
 def use_fits(ftp,tokens=[]):
-    print('Using files from: ' ftp.pwd())
+    print('\n\nUsing clean fits files from: ' + ftp.pwd()+'\n\n')
+    files = ftp.nlst()
+    for f in files:
+        if 'clean.fits' in f:
+            print(f)
+            ftp.retrbinary('RETR '+f, open('input_data/'+f, 'wb').write)
+
 
 def quit_ftp(ftp,tokens):
     ftp.close()
 
 
 def parse_input(ftp,inp):
-    cmds = ['help','ls','cd','use','q']
+    cmds = ['help\n','ls\n','cd','use\n','q\n']
     funcs = [print_help, list_dir, change_dir, use_fits, quit_ftp]
     tokens = inp.split(' ')
+    print(tokens)
     if tokens[0] in cmds:
-        func[cmds.index(tokens[0])](ftp,tokens)
-        return tokens[0]
+        funcs[cmds.index(tokens[0])](ftp,tokens)
     else:
         print_help()
+    return tokens[0]
 
 
 def handle_ftp(ftp):
     list_dir(ftp)
-    while line in sys.stdin:
-        parse_input(ftp, line):
+    print(':' + ftp.pwd() + '$ ',end='',flush=True)
+    while 1:
+        try:
+            line =sys.stdin.readline()
+        except KeyBoardInterrupt:
+            break
+        if not line:
+            break
+
+        if(parse_input(ftp, line)=='q\n'):
+            return
+        print(':'+ftp.pwd()+'$ ',end='',flush=True)
 
 
 
@@ -60,12 +81,7 @@ def main():
     # load_file()
     login_ftp()
 
-def load_file(f=r'D:\NEOSSAT Data\ASTRO\2018\033\2002_AJ129\FINE_POINT\NEOS_SCI_2018033163030_clean.fits'):
-    image = fits.open(f)
-    image.info()
-
-def main():
-   load_file()
+##TODO: Integrate the asteroid finder through program
 
 if __name__ == "__main__":
     main()
